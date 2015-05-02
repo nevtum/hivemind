88ce39a0262b404b53430920c2932307251398ce
Neville Tummon <nt.devs@gmail.com>
2015-05-02 19:35:06 +1000

    bootstrapped django rest framework

--- echelon/settings.py
+++ echelon/settings.py
@@ -40,8 +40,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'clients'
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- echelon/urls.py
+++ echelon/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'echelon.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/register/$', 'clients.views.create_company', name='newclient'),
     url(r'^clients/$', 'clients.views.index'),
     url(r'^clients/([a-zA-Z]+)/$', 'clients.views.contacts'),
+
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
