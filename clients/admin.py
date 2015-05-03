5e69c41fcc2af2c6f7f54dea7cd569bb4ef93d53
Neville Tummon <nt.devs@gmail.com>
2015-05-03 22:16:00 +1000

    added workrole to items editable in admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from clients.models import Company, Contact
+from clients.models import Company, Contact, WorkRole
 
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(Contact)
+admin.site.register(WorkRole)
