c3c2723b9827409c60df29b76e005047dc4a6508
Neville Tummon <nt.devs@gmail.com>
2015-05-10 18:47:13 +1000

    fixes to view rest framework static files
    The reason for this change was when running app via gunicorn files
    from the static folder such as css and js would not be served up.
    The change enables django rest framework to serve its static files
    in development mode. To serve static files in production mode it
    is advised to use nginx to handle static files and gunicorn to
    handle the application specific files.

--- echelon/urls.py
+++ echelon/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
