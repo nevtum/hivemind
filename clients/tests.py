6b1e0290217f277f1072a22c5a88dc896572b2de
Neville Tummon <nt.devs@gmail.com>
2015-05-10 21:59:48 +1000

    testing command handler api

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class CommandHandlerTests(APITestCase):
+    def test_invalid_header(self):
+        self.client.force_authenticate(user=None)
+        data = { 'id': 123 }
+        response = self.client.post('/clients/v1/commands/', data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.data.get('request-data'), data)
