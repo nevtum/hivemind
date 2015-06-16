69e10fb20e46de88e4a28e74ec2506bd279bbd40
Neville Tummon <nt.devs@gmail.com>
2015-06-16 20:20:06 +1000

    enabled token authentication

--- src/echelon/settings/drf.py
+++ src/echelon/settings/drf.py
@@ -1,8 +1,15 @@
 from .common import *
 
-INSTALLED_APPS += ['rest_framework']
+INSTALLED_APPS += [
+    'rest_framework',
+    'rest_framework.authtoken'
+]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
--- src/echelon/urls.py
+++ src/echelon/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
+    url(r'^login/', views.obtain_auth_token),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
