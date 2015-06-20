3c43560af2939277961a2907c3ffab9fa3392abb
Neville Tummon <nt.devs@gmail.com>
2015-06-20 22:54:34 +1000

    added template to dirt page

--- src/dirts/views.py
+++ src/dirts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from dirts.models import Defect
 
 # Create your views here.
 
 def index(request):
-    from django.http import HttpResponse
-    return HttpResponse("DIRTs page!")
+    defects = Defect.objects.all().order_by('-date_created')
+    return render(request, 'dirt_list.html', {'defects': defects})
