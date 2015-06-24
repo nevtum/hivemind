d3e026c4393978e1de8e350998b59705a34ec187
Neville Tummon <nt.devs@gmail.com>
2015-06-24 19:22:46 +1000

    should not update all fields when amending dirt

--- src/dirts/services/dirt_manager.py
+++ src/dirts/services/dirt_manager.py
@@ -8,26 +8,6 @@ def get_detail(dirt_id):
 
 def raise_dirt(**kwargs):
     defect = Defect()
-    _save(defect, **kwargs)
-
-def amend_dirt(dirt_id, **kwargs):
-    defect = Defect.objects.get(id=dirt_id)
-
-    if defect.status.name != "Open":
-        raise Exception("DIRT must be in open state to amend.")
-
-    _save(defect, **kwargs)
-
-def mark_accepted(dirt_id):
-    pass
-
-def mark_rejected(dirt_id, reason):
-    pass
-
-def close_dirt(dirt_id):
-    pass
-
-def _save(defect, **kwargs):
     defect.project_code = kwargs['project_code']
     defect.date_created = kwargs['date_created']
     defect.submitter = kwargs['submitter']
@@ -37,3 +17,25 @@ def _save(defect, **kwargs):
     defect.description = kwargs['description']
     defect.reference = kwargs['reference']
     defect.save()
+
+def amend_dirt(dirt_id, **kwargs):
+    defect = Defect.objects.get(id=dirt_id)
+
+    if defect.status.name != "Open":
+        raise Exception("DIRT must be in open state to amend.")
+
+    defect.project_code = kwargs['project_code']
+    defect.release_id = kwargs['release_id']
+    defect.title = kwargs['title']
+    defect.description = kwargs['description']
+    defect.reference = kwargs['reference']
+    defect.save()
+
+def mark_accepted(dirt_id):
+    pass
+
+def mark_rejected(dirt_id, reason):
+    pass
+
+def close_dirt(dirt_id):
+    pass
