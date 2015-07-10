754279cf1ba33adc9fd459729c2e0c5b841306cf
Neville Tummon <nt.devs@gmail.com>
2015-07-10 11:24:57 +1000

    updated release version

--- src/echelon/settings/common.py
+++ src/echelon/settings/common.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Attention!!
 # Update this settings variable when releasing a new version of Echelon
-RELEASE_VERSION = 'v0.1.3'
+RELEASE_VERSION = 'v0.1.4'
 
 # Build paths inside the project like this: os.path.join(SRC_DIR, ...)
 import os
