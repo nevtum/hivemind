6b82612ecd534c72bf7020e90519e288d505ec00
Neville Tummon <nt.devs@gmail.com>
2015-07-14 19:01:55 +1000

    fixed few tests to force user authentication

--- src/clients/tests.py
+++ src/clients/tests.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 
 class CommandHandlerTests(APITestCase):
+    def setUp(self):
+        user = User()
+        self.client.force_authenticate(user)
+
     def test_invalid_header(self):
-        self.client.force_authenticate(user=None)
         data = { 'id': 123 }
         response = self.client.post('/clients/v1/commands/', data, format='json')
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
