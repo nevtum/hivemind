946a9cde6eee7a98b988028c8ff2069c0b46ec9d
Neville Tummon <nt.devs@gmail.com>
2015-11-22 14:04:11 +1100

    using drf javascript web token

--- src/echelon/settings/drf.py
+++ src/echelon/settings/drf.py
@@ -1,15 +1,15 @@
 from .common import INSTALLED_APPS
 
 INSTALLED_APPS += [
-    'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
 }
--- src/echelon/urls.py
+++ src/echelon/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^$', 'echelon.views.index', name='home-url'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
     url(r'^dirts/', include('dirts.urls')),
-    url(r'^auth-token/', views.obtain_auth_token),
+    url(r'^auth-token/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^login/', 'echelon.views.log_in', name='login-url'),
     url(r'^logout/', 'echelon.views.log_out', name='logout-url'),
 ]
