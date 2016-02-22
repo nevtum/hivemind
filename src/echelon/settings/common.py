fe7feedc96629e693eec977b35c89a7e5b478286
Neville Tummon <nt.devs@gmail.com>
2016-02-22 15:48:13 +1100

    cleaner form styles with django crispy forms

--- src/echelon/settings/common.py
+++ src/echelon/settings/common.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'crispy_forms'
 ]
 
 INSTALLED_APPS += [
