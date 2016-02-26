6cc19bd4cdedfe6a87993a31017896299f169693
Neville Tummon <nt.devs@gmail.com>
2016-02-26 21:42:07 +1100

    refactor to cleaner tests

--- src/dirts/tests.py
+++ src/dirts/tests.py
@@ -16,7 +16,6 @@ class DefectAcceptanceTests(TestCase):
         Priority.objects.create(name='Low')
         Priority.objects.create(name='Observational')
     
-    
     def _login_fake_user(self):
         username = 'test_user'
         email = 'test@test.com'
@@ -24,17 +23,10 @@ class DefectAcceptanceTests(TestCase):
         self.test_user = User.objects.create_user(username, email, password)
         login = self.client.login(username='test_user', password='test_password')
         self.assertEquals(login, True)
-    
-    def setUp(self):
-        self._load_fixtures()
-        self._login_fake_user()
-        
-    def test_should_open_correct_form_when_create_defect_url_accessed(self):
-        response = self.client.get(reverse('create-dirt-url'))
-        self.assertIsInstance(response.context['form'], CreateDirtForm)
-        
-    def test_should_create_new_defect(self):
-        kwargs = {
+
+    @staticmethod
+    def _test_form_data_with_comments():
+        return {
             'project_code': 'ABC.123',
             'release_id': 'v1.2.3.4',
             'priority': Priority.objects.get(name='High').id,
@@ -42,12 +34,39 @@ class DefectAcceptanceTests(TestCase):
             'description': 'some defect description',
             'comments': 'some comments',
         }
-        form = CreateDirtForm(kwargs)
+    
+    @staticmethod
+    def _test_form_data_without_comments():
+        return {
+            'project_code': 'ABC.123',
+            'release_id': 'v1.2.3.4',
+            'priority': Priority.objects.get(name='High').id,
+            'reference': 'a title',
+            'description': 'some defect description',
+        }
+
+    def setUp(self):
+        self._load_fixtures()
+        self._login_fake_user()
+        
+    def test_should_open_correct_form_when_create_defect_url_accessed(self):
+        response = self.client.get(reverse('create-dirt-url'))
+        self.assertIsInstance(response.context['form'], CreateDirtForm)
+    
+    def test_should_pass_valid_create_defect_form_with_comments(self):
+        form = CreateDirtForm(data=self._test_form_data_with_comments())
+        self.assertEquals(form.is_valid(), True)
+    
+    def test_should_pass_valid_create_defect_form_without_comments(self):
+        form = CreateDirtForm(data=self._test_form_data_without_comments())
+        self.assertEquals(form.is_valid(), True)
+    
+    def test_should_create_new_defect(self):
+        form = CreateDirtForm(data=self._test_form_data_with_comments())
         defect = form.save(commit=False)
         defect.submitter = self.test_user
         defect.raise_new()
         
-        self.assertEquals(form.is_valid(), True)
         self.assertEquals(defect.project_code, 'ABC.123')
         self.assertEquals(defect.release_id, 'v1.2.3.4')
         self.assertEquals(defect.status.name, 'Open')
@@ -57,15 +76,7 @@ class DefectAcceptanceTests(TestCase):
         self.assertEquals(defect.comments, 'some comments')
         
     def test_should_create_dirt_opened_event(self):
-        kwargs = {
-            'project_code': 'ABC.123',
-            'release_id': 'v1.2.3.4',
-            'priority': Priority.objects.get(name='High').id,
-            'reference': 'a title',
-            'description': 'some defect description',
-            'comments': 'some comments',
-        }
-        form = CreateDirtForm(kwargs)
+        form = CreateDirtForm(data=self._test_form_data_with_comments())
         defect = form.save(commit=False)
         defect.submitter = self.test_user
         event = defect.raise_new()
