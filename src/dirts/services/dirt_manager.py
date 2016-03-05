78817b7655cdf4b393ba2395edc90175f828c11e
Neville Tummon <nt.devs@gmail.com>
2016-03-06 10:40:40 +1100

    fixed duplicate entries appearing in dirt list
    Whenever an item would have 2 or more tags attached to it, it
    would end up appearing just as many times as there are tags. This
    bug has been fixed if no search string is provided.

--- src/dirts/services/dirt_manager.py
+++ src/dirts/services/dirt_manager.py
@@ -3,6 +3,9 @@ from dirts.models import Defect
 from common import store as EventStore
 
 def latest_dirts(keyword):
+    if not keyword:
+        return Defect.objects.latest()
+    
     query = Q(reference__icontains=keyword) \
     | Q(project_code__icontains=keyword) \
     | Q(description__icontains=keyword) \
@@ -10,7 +13,7 @@ def latest_dirts(keyword):
     | Q(release_id__icontains=keyword) \
     | Q(tags__name__in=[keyword])
 
-    return Defect.objects.latest().filter(query)
+    return Defect.objects.latest().filter(query)    
 
 def delete_dirt(dirt_id, user):
     defect = Defect.objects.get(pk=dirt_id).as_domainmodel()
