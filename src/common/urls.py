580330119e88140dfc23d394bfad94a17e01f57d
Neville Tummon <nt.devs@gmail.com>
2016-04-03 01:48:19 +1100

    must be logged in to use new views

--- src/common/urls.py
+++ src/common/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required as auth
 from common.handler_views import accept
 from .views import CreateProjectView, EditProjectView, ProjectListView
 
 urlpatterns = [
     url(r'^v1/command-handler/$', accept, name='commands'),
     url(r'^projects/$', ProjectListView.as_view(), name='projects'),
-    url(r'^projects/new/$', CreateProjectView.as_view(), name='create-project'),
-    url(r'^projects/(?P<slug>[-\w\d]+)/$', EditProjectView.as_view(), name='edit-project'),
+    url(r'^projects/new/$', auth(CreateProjectView.as_view()), name='create-project'),
+    url(r'^projects/(?P<slug>[-\w\d]+)/$', auth(EditProjectView.as_view()), name='edit-project'),
 ]
