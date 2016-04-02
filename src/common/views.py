f013740b7d64686fd4ebb09b5e6752f962a33d25
Neville Tummon <nt.devs@gmail.com>
2016-04-02 14:58:30 +1100

    render list of projects

--- src/common/views.py
+++ src/common/views.py
@@ -2,5 +2,5 @@ from .models import Project
 from django.views.generic import ListView, UpdateView, CreateView
 
 class ProjectListView(ListView):
-    model = Project
+    queryset = Project.objects.all().order_by('-date_created')
     template_name = 'project_list.html'
