b0e2749eeb02e4f071ee74c5b367ac211e323a43
Neville Tummon <nt.devs@gmail.com>
2016-04-03 01:34:01 +1100

    added view to add new project

--- src/common/urls.py
+++ src/common/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from common.handler_views import accept
-from .views import ProjectListView
+from .views import CreateProjectView, ProjectListView
 
 urlpatterns = [
     url(r'^v1/command-handler/$', accept, name='commands'),
     url(r'^projects/$', ProjectListView.as_view(), name='projects'),
+    url(r'^projects/new/$', CreateProjectView.as_view(), name='create-project'),
+    
 ]
--- src/common/views.py
+++ src/common/views.py
@@ -1,5 +1,17 @@
 from .models import Project
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, UpdateView, CreateView
+
+class CreateProjectView(CreateView):
+    model = Project
+    template_name = 'edit_project.html'
+    success_url=reverse_lazy('projects')
+    fields = [
+        'manufacturer',
+        'code',
+        'description',
+        'date_created',
+    ]
 
 class ProjectListView(ListView):
     queryset = Project.objects.all().order_by('-date_created')
