8d485a3e3cfb2a0ceee92ea85594861e0cff8821
Neville Tummon <nt.devs@gmail.com>
2016-09-11 23:19:43 +1000

    can now link to any django model

--- src/feed/migrations/0001_initial.py
+++ src/feed/migrations/0001_initial.py
@@ -8,18 +8,20 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0002_remove_content_type_name'),
         ('common', '0004_manufacturer_project'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Activity',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
                 ('summary', models.CharField(max_length=80)),
-                ('url', models.URLField()),
                 ('date_occurred', models.DateTimeField(auto_now_add=True)),
+                ('object_id', models.PositiveIntegerField()),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
                 ('project', models.ForeignKey(to='common.Project')),
                 ('submitter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
--- src/feed/models.py
+++ src/feed/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from common.models import Project
 
 class Activity(models.Model):
     project = models.ForeignKey(Project)
     summary = models.CharField(max_length=80)
-    url = models.URLField()
     date_occurred = models.DateTimeField(auto_now_add=True)
     submitter = models.ForeignKey(User)
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
