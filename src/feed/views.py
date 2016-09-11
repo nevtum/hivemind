9099ae16463de9f5f74d2007553bfbb43f1fba40
Neville Tummon <nt.devs@gmail.com>
2016-09-11 15:54:26 +1000

    some styling for activities

--- src/feed/views.py
+++ src/feed/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from .models import Activity
 
 class ActivityListView(ListView):
-    model = Activity
+    queryset = Activity.objects.order_by('-date_occurred')
     template_name = 'feed.html'
     context_object_name = 'feed'
     paginate_by = 50
