2793abf17b632243d0ad4fd3fb95233c68e17dc0
Neville Tummon <nt.devs@gmail.com>
2016-09-25 23:42:45 +1000

    admin to activity objects a bit neater

--- src/feed/admin.py
+++ src/feed/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Activity
 
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('date_occurred', 'project', 'summary', 'submitter',)
+
 # Register your models here.
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
