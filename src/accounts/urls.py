37b93d3b5be87f5204a14e9108f7c2ff43d0ecb6
Neville Tummon <nt.devs@gmail.com>
2016-10-21 08:41:53 +1100

    simple admin page to view pending registrations

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^register/$', views.SignupRequestView.as_view(), name='register'),
     url(r'^completed/$', views.register_complete, name='thanks'),
+    url(r'^requests_pending/$', views.SignupListView.as_view(), name='pending'),    
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import redirect, render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from .forms import SignupForm
 from .models import SignupRequest
@@ -19,3 +19,8 @@ class SignupRequestView(CreateView):
 
 def register_complete(request):
     return render(request, 'wait_admin.html')
+
+class SignupListView(ListView):
+    queryset = SignupRequest.objects.filter(pending_approval=True)
+    context_object_name = 'registrations'
+    template_name = 'registrations.html'
