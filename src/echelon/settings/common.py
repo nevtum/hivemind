a8b9fb249a7209800f94861df85968b3d9045b87
Neville Tummon <nt.devs@gmail.com>
2016-10-26 00:40:53 +1100

    update release version

--- src/echelon/settings/common.py
+++ src/echelon/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Attention!!
 # Update this settings variable when releasing a new version of Echelon
 # such as when there are changes to db models
-RELEASE_VERSION = 'v0.2.8'
+RELEASE_VERSION = 'v0.2.9'
 
 # Build paths inside the project like this: os.path.join(SRC_DIR, ...)
 import os
