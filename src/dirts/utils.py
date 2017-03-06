2e098dda665b20758dbb508110cc24d62ef63e03
Neville Tummon <nt.devs@gmail.com>
2017-03-06 11:47:30 +1100

    getting closer

--- src/dirts/utils.py
+++ src/dirts/utils.py
@@ -1,9 +1,19 @@
+import csv
+from io import StringIO
+
+def json_from(contents):
+    data = StringIO(contents.read().decode('utf-8'))
+    reader = csv.DictReader(data, delimiter=',')
+    for row in reader:
+        yield row
 
 def import_data(contents):
     """A function which reads converts the contents
     of a CSV file into an array of JSON serializable
     properties, validated and ready for
     conversion to Defect model objects"""
+    return [item for item in json_from(contents)]
+
     return [
         {
             'date_created': '3/03/2017',
