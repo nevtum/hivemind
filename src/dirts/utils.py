c6218f73e83610dd4f268841dfa7ce7c6b6c1940
Neville Tummon <nt.devs@gmail.com>
2017-03-06 22:11:26 +1100

    extracting datetime data

--- src/dirts/utils.py
+++ src/dirts/utils.py
@@ -1,16 +1,32 @@
 import csv
+import re
 from io import StringIO
 
-def json_from(request):
+from django.utils.timezone import datetime
+
+from .forms import CreateDirtForm
+
+def parse_datetime(datestring) -> datetime:
+    expr = re.compile('^(?P<day>\d{1,2})\/(?P<month>\d{1,2})\/(?P<year>\d{4})$')
+    match = expr.match(datestring)
+    if not match:
+        raise ValueError('Incorrect date format. Should be dd/mm/yyyy.')
+    kw = match.groupdict()
+    day = int(kw['day'])
+    month = int(kw['month'])
+    year = int(kw['year'])
+    return datetime(year, month, day)
+
+def json_from(request) -> list:
     contents = request.FILES['import_file']
     code = request.POST['project_code']
     submitter = request.user.username
-    # import pdb; pdb.set_trace()
     data = StringIO(contents.read().decode('utf-8'))
     reader = csv.DictReader(data, delimiter=',')
     for row in reader:
+        date_created = parse_datetime(row['Date Created'])
         yield {
-            'date_created': row['Date Created'],
+            'date_created': date_created,
             'description': row['Description'],
             'comments': row['Comments'],
             'priority': row['Priority'],
@@ -22,9 +38,17 @@ def json_from(request):
             'project_code': code
         }
 
+def validated(input: list) -> list:
+    for data in input:
+        # form = CreateDirtForm(**data)
+        yield data
+
+
 def import_data(request):
     """A function which reads converts the contents
     of a CSV file into an array of JSON serializable
     properties, validated and ready for
     conversion to Defect model objects"""
-    return [item for item in json_from(request)]
+    mapping = json_from(request)
+    mapping = validated(mapping)
+    return [item for item in mapping]
