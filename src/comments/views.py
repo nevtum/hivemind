42b59f5ad0a3190e76f8fc429689e8c87c1e8228
Neville Tummon <nt.devs@gmail.com>
2017-05-25 08:40:47 +1000

    simple rendering working

--- src/comments/views.py
+++ src/comments/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def comments_for_defect(request, pk):
     comments = Comment.objects.filter(defect__id=pk)
-    return render(request, 'defect_comments.html', comments)
+    return render(request, 'defect_comments.html', {'comments': comments})
