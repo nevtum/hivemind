f45ed719441c7981dfda11b7765ab279f361b0c2
Neville Tummon <nt.devs@gmail.com>
2017-05-27 01:46:17 +1000

    can delete comments

--- src/comments/views.py
+++ src/comments/views.py
@@ -16,12 +16,25 @@ class CommentEditView(UpdateView):
         comment = form.save(commit=False)
         # set toggle that comment is edited
         comment.save()
-        return redirect(reverse('comments:list', kwargs={'pk': comment.defect.id}))
-
-def edit_comment(request, pk):
-    comment = Comment.objects.get(pk=pk)
-    return render(request, 'edit_comment.html', { 'comment': comment })
+        return redirect(reverse('comments:list', kwargs={ 'pk': comment.defect.id }))
 
 def delete_comment(request, pk):
     comment = Comment.objects.get(pk=pk)
-    return render(request, 'delete_comment.html', { 'comment': comment })
+    if request.method == 'GET':
+        return render(request, 'delete_comment.html', { 'comment': comment })
+    
+    defect_id = comment.defect.id
+    comment.delete()
+    return redirect(reverse('comments:list', kwargs={ 'pk': defect_id }))
+
+
+
+    # if request.method == 'GET':
+    #     return render(request, 'delete_confirmation.html', {'id': pk})
+
+    # defect = Defect.objects.get(pk=pk)
+    # defect_model = defect.as_domainmodel()
+    # event = defect_model.soft_delete(request.user, timezone.now())
+    # EventStore.append_next(event)
+    # defect.delete()
+    # return redirect('dirts-list')
