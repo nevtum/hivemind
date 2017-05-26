d09c4bcd525219fc420f555bbe4a23312a003350
Neville Tummon <nt.devs@gmail.com>
2017-05-26 22:28:57 +1000

    can post new comments

--- src/comments/views.py
+++ src/comments/views.py
@@ -1,4 +1,5 @@
 from .models import Comment
+from dirts.models import Defect
 from django.shortcuts import render, redirect
 
 def comments_for_defect(request, pk):
@@ -10,6 +11,9 @@ def comments_for_defect(request, pk):
     return render(request, 'defect_comments.html', res)
 
 def add_comment_for_defect(request, pk):
-    content = request.POST.get('newcomment')
-    import pdb; pdb.set_trace()
+    comment = Comment()
+    comment.content = request.POST.get('newcomment')
+    comment.author = request.user
+    comment.defect = Defect.objects.get(pk=pk)
+    comment.save()
     return redirect('comments:list', pk)
