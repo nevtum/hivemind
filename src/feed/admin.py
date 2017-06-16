17b1b1edbc5394914dac2fddd1be15df78120763
Neville Tummon <nt.devs@gmail.com>
2017-06-16 10:44:52 +1000

    remove redundant app. not likely to use

--- src/feed/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Activity
-
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('date_occurred', 'project', 'summary', 'submitter',)
-
-# Register your models here.
-admin.site.register(Activity, ActivityAdmin)
--- src/feed/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Activity
-
-class ActivityListView(ListView):
-    queryset = Activity.objects.order_by('-date_occurred')
-    template_name = 'feed.html'
-    context_object_name = 'feed'
-    paginate_by = 50
