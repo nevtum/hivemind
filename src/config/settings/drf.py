bf8a1395473e50ebd6a8d0ca2fdd05157239c7fa
Neville Tummon <nt.devs@gmail.com>
2017-06-22 00:30:05 +1000

    adding api wide throttling

--- src/config/settings/drf.py
+++ src/config/settings/drf.py
@@ -10,5 +10,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/minute',
+        'user': '200/minute'
+    }
 }
