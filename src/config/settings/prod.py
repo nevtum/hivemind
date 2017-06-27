f2320c7e1ab554701dfde412e71222557b2363ae
Neville Tummon <nt.devs@gmail.com>
2017-06-27 21:01:06 +1000

    turn off debugging in prod since logger is working

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -2,7 +2,7 @@ import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
