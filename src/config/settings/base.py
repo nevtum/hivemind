7664225150947d13ef5016120b859f4813b7176a
Neville Tummon <nt.devs@gmail.com>
2017-08-15 15:44:40 +1000

    update release version

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Attention!!
 # Update this settings variable when releasing a new version of Echelon
 # such as when there are changes to db models
-RELEASE_VERSION = 'v0.6.3'
+RELEASE_VERSION = 'v0.6.4'
 
 # Build paths inside the project like this: os.path.join(SRC_DIR, ...)
 import os
