50c41d866b2757bdaaac00d14dbc6f95b98c5a79
Neville Tummon <nt.devs@gmail.com>
2017-08-15 15:44:02 +1000

    fix for empty project_code inputs

--- src/api/utils.py
+++ src/api/utils.py
@@ -3,11 +3,10 @@ from .core.domain.request import FilterListRequest
 from common.models import Project
 
 def defect_activities(project_code, search_param=''):
-    project_id = Project.objects.get(code=project_code).id
     story = DomainEventFilterUserStory()
     request_object = FilterListRequest.from_dict({
         'clients': [],
-        'projects': [project_id],
+        'projects': projects_array(project_code),
         'usernames': [],
         'search': {
             'q': search_param,
@@ -18,3 +17,8 @@ def defect_activities(project_code, search_param=''):
     if response.has_errors:
         raise ValueError(response.message)
     return response.value
+
+def projects_array(project_code):
+    if project_code == '':
+        return []
+    return [Project.objects.get(code=project_code).id]
