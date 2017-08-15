37e6a4a7fd35b41367b055c1724dda73e6695f14
Neville Tummon <nt.devs@gmail.com>
2017-08-15 14:46:29 +1000

    fix for util function used by api

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,11 +1,13 @@
 from .core.domain.user_stories import DomainEventFilterUserStory
 from .core.domain.request import FilterListRequest
+from common.models import Project
 
-def defect_activities(code, search_param=''):
+def defect_activities(project_code, search_param=''):
+    project_id = Project.objects.get(code=project_code).id
     story = DomainEventFilterUserStory()
     request_object = FilterListRequest.from_dict({
         'clients': [],
-        'projects': [code],
+        'projects': [project_id],
         'usernames': [],
         'search': {
             'q': search_param,
